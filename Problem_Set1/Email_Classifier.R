7927def4712e690c42aa006703f2b4e1d7f68e30
Pat Ellis <ellispat@msu.edu>
2014-02-13 13:46:25 -0500

    Update and rename Email_Classifier to Email_Classifier.R

---
+++ Problem_Set1/Email_Classifier.R
@@ -0,0 +1,6 @@
+
+# File-Name:       email_Classifier.R           
+# Due:             02/28/14                                
+# By:              Patrick Ellis
+# Prompt:          Please Replicate the Spam versus Ham Classifier Example from Chapter 3 of Machine Learning for Hackers  
+# Data Used:       Email messages contained in data/ directory, source: http://spamassassin.apache.org/publiccorpus/
