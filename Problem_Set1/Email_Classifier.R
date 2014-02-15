b2b257aa50918b7ef075a06d55d4b83e1d65e4a4
Pat Ellis <ellispat@msu.edu>
2014-02-15 00:07:33 -0500

    Update Email_Classifier.R

--- Problem_Set1/Email_Classifier.R
+++ Problem_Set1/Email_Classifier.R
@@ -1,5 +1,4 @@
-
-# File-Name:       email_Classifier.R           
+# File-Name:       Email_Classifier.R           
 # Due:             02/28/14                                
 # By:              Patrick Ellis
 # Prompt:          Please Replicate the Spam versus Ham Classifier Example from Chapter 3 of Machine Learning for Hackers  
