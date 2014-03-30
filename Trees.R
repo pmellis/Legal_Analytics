4ff5fb299ca2c9229978a3b8f7ff7e91d7033661
Pat Ellis <ellispat@msu.edu>
2014-03-30 19:20:53 -0400

    Update and rename Trees to Trees.R

--- Trees.R
+++ Trees.R
@@ -1,7 +1,7 @@
 # File-Name:       Trees.R           
 # Due:             03/31/14                                
 # By:              Patrick Ellis
-# Prompt:          Replicate the classification in this <a href="https://www.youtube.com/watch?v=kdUVUINpFmI">video</a>       
+# Prompt:          Replicate the classification in this video: https://www.youtube.com/watch?v=kdUVUINpFmI.    
 # Data Used:       Iris Data in R
 # Results:         
 
